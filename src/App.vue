0b31cbb071270f9ed61ea68640f84510310ad71f
liujie <1509582688@qq.com>
2018-10-30 08:40:01 +0800

    'create'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello word
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~stylus/variable.styl'
+    #app
+      color $color-theme
+</style>
+
